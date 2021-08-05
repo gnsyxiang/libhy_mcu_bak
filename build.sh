ade38d8f1975df10ed2637b2e600bdbb698fc5dd
zhenquan.qiu <gnsyxiang@163.com>
2021-08-05 09:05:43 +0800

    feat: add protobuf-c demo

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [ x$1 = x"mcu" ]; then
     gcc_prefix=arm-none-eabi
     cross_gcc_path=${data_disk_path}/opt/toolchains/${vender}/${gcc_version}/bin/${gcc_prefix}-
     _ldflag_com="-specs=nano.specs -specs=nosys.specs"
-    _param_com="--enable-at32f407vgt7"
+    _param_com="--enable-at32f407vgt7 --enable-libprotobuf_c"
 else
     help_info
 fi
