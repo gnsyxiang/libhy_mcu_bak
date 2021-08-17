c6d0ea060c959ee9fd120a8eb403d04a9df73c86
zhenquan.qiu <gnsyxiang@163.com>
2021-08-17 19:56:12 +0800

    feat: add hy_func_check files

---
+++ include/hy_func_check.h
@@ -0,0 +1,34 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_func_check.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    17/08 2021 19:24
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        17/08 2021      create the file
+ * 
+ *     last modified: 17/08 2021 19:24
+ */
+#ifndef __LIBHY_MCU_INCLUDE_HY_FUNC_CHECK_H_
+#define __LIBHY_MCU_INCLUDE_HY_FUNC_CHECK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void HyFuncCheck(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/hc32l13x/hy_func_check.c
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_func_check.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    17/08 2021 19:23
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        17/08 2021      create the file
+ * 
+ *     last modified: 17/08 2021 19:23
+ */
+#include <stdio.h>
+
+#include "hy_func_check.h"
+
+void HyFuncCheck(void)
+{
+
+}
